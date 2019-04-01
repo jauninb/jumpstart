cd76b9153359bf5c4e431851f8420f7c9c8bb628
jaunin-b <jaunin.b@fr.ibm.com>
2019-04-01 09:34:01 +0200

    Rename cluster namesapce to identity it is the build one

--- setup_dind.sh
+++ setup_dind.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export BUILD_CLUSTER=${BUILD_CLUSTER:-"bp2i"}
-export CLUSTER_NAMESPACE=${CLUSTER_NAMESPACE:-"build"}
+export BUILD_CLUSTER_NAMESPACE=${BUILD_CLUSTER_NAMESPACE:-"build"}
 export IBMCLOUD_TARGET_REGION=${IBMCLOUD_TARGET_REGION:-"eu-gb"}
 
 echo "Logging in to build cluster account..."
@@ -20,22 +20,22 @@ CLUSTER_CONFIG_COMMAND=$(ibmcloud ks cluster-config -cluster "$BUILD_CLUSTER" --
 echo "$CLUSTER_CONFIG_COMMAND"
 eval $CLUSTER_CONFIG_COMMAND
 
-echo "Checking cluster namespace $CLUSTER_NAMESPACE"
-if ! kubectl get namespace "$CLUSTER_NAMESPACE"; then
-  kubectl create namespace "$CLUSTER_NAMESPACE"
+echo "Checking cluster namespace $BUILD_CLUSTER_NAMESPACE"
+if ! kubectl get namespace "$BUILD_CLUSTER_NAMESPACE"; then
+  kubectl create namespace "$BUILD_CLUSTER_NAMESPACE"
 fi
 
 # Ensure there is a Docker server on the ${BUILD_CLUSTER}
-if ! kubectl --namespace "$CLUSTER_NAMESPACE" rollout status -w deployment/docker; then
+if ! kubectl --namespace "$BUILD_CLUSTER_NAMESPACE" rollout status -w deployment/docker; then
   echo "Installing Docker Server into build cluster..."
-  kubectl --namespace "$CLUSTER_NAMESPACE" run docker --image=docker:dind --overrides='{ "apiVersion": "apps/v1", "spec": { "template": { "spec": {"containers": [ { "name": "docker", "image": "docker:dind", "securityContext": { "privileged": true } } ] } } } }'
-  kubectl --namespace "$CLUSTER_NAMESPACE" rollout status -w deployment/docker
+  kubectl --namespace "$BUILD_CLUSTER_NAMESPACE" run docker --image=docker:dind --overrides='{ "apiVersion": "apps/v1", "spec": { "template": { "spec": {"containers": [ { "name": "docker", "image": "docker:dind", "securityContext": { "privileged": true } } ] } } } }'
+  kubectl --namespace "$BUILD_CLUSTER_NAMESPACE" rollout status -w deployment/docker
 fi
 
 # Use port-forward to make the pod/port locally accessible
 # Be sure to use a running POD (not an evicted one)
-kubectl --namespace "$CLUSTER_NAMESPACE" get pods 
-kubectl --namespace "$CLUSTER_NAMESPACE" port-forward $(kubectl --namespace "$CLUSTER_NAMESPACE" get pods | grep docker | grep -i running | awk '{print $1;}') 2375:2375 > /dev/null 2>&1 &
+kubectl --namespace "$BUILD_CLUSTER_NAMESPACE" get pods 
+kubectl --namespace "$BUILD_CLUSTER_NAMESPACE" port-forward $(kubectl --namespace "$BUILD_CLUSTER_NAMESPACE" get pods | grep docker | grep -i running | awk '{print $1;}') 2375:2375 > /dev/null 2>&1 &
 
 while ! nc -z localhost 2375; do   
   sleep 0.1
