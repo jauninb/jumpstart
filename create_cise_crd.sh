6242be3468f97af3c31453993d073e3aeb2c578f
Benoit Jaunin <jaunin.b@fr.ibm.com>
2019-06-07 18:33:47 +0200

    Add CISE crd generator

---
+++ create_cise_crd.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# uncomment to debug the script
+# set -x
+
+cise_crd_template=$(cat <<'EOT'
+ apiVersion: securityenforcement.admission.cloud.ibm.com/v1beta1
+ kind: %s
+ metadata:
+   name: %s
+ spec:
+    repositories:
+     - name: %s
+       policy:
+         trust:
+           enabled: %s
+           signerSecrets:
+           - name: %s
+         va:
+           enabled: %s
+EOT
+)
+
+printf "$cise_crd_template" \
+  "ImagePolicy" \
+  "$REGISTRY_NAMESPACE.$IMAGE_NAME.$DEVOPS_SIGNER" \
+  "$REGISTRY_URL/$REGISTRY_NAMESPACE/$IMAGE_NAME" \
+  "true" \
+  "$REGISTRY_NAMESPACE.$IMAGE_NAME.$DEVOPS_SIGNER" \
+  "true"
