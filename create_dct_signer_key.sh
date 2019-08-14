bdd96b23c7fb4ae26dde559d4e74b818dadd9c98
Hua Yuen <huayuenh@ie.ibm.com>
2019-08-14 15:12:37 +0100

    remove tar

--- create_dct_signer_key.sh
+++ create_dct_signer_key.sh
@@ -29,7 +29,6 @@ if [[ "$ARCHIVE_DIR" ]]; then
     echo "DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE=$DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE" >> $ARCHIVE_DIR/dct.properties
     cp *.pub $ARCHIVE_DIR
     docker trust inspect $GUN | jq -r --arg GUN "$GUN" --arg DEVOPS_SIGNER "$DEVOPS_SIGNER" '.[] | select(.name=$GUN) | .Signers' > $ARCHIVE_DIR/dct_signers.json
-    umask 077; tar -zcvf $ARCHIVE_DIR/private_keys_backup.tar.gz --directory ~ .docker/trust/private; umask 022
 else 
     # No ARCHIVE_DIR so echo the information required to configure DCT
     # Look for the private key file generated for the devops_signer
