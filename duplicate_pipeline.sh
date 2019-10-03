cc1e34c7f12546761e784ee70769e0ec006c4934
Benoit Jaunin <jaunin.b@fr.ibm.com>
2019-10-03 14:49:42 +0200

    Update the pipeline duplication to use yq provided in pipeline base image https://github.com/mikefarah/yq

--- duplicate_pipeline.sh
+++ duplicate_pipeline.sh
@@ -2,7 +2,7 @@
 # uncomment to debug the script
 # set -x
 # This script does a duplication of a pipeline into an existing empty pipeline
-# It requires cURL, jq (https://stedolan.github.io/jq/) and yq (https://yq.readthedocs.io/en/latest/) available
+# It requires cURL, jq (https://stedolan.github.io/jq/) and yq (https://github.com/mikefarah/yq) available
 # You must be logged-in to the account and the region that the toolchain/pipelines you want to duplicate from/to
 # are hosted
 
@@ -30,27 +30,31 @@ cat "${SOURCE_PIPELINE_ID}.yaml"
 # Find the token url for the git tile
 curl -H "Authorization: $BEARER_TOKEN" -H "Content-Type: application/json" -o "${SOURCE_PIPELINE_ID}_inputsources.json" "$PIPELINE_API_URL/pipelines/$SOURCE_PIPELINE_ID/inputsources"
 
+# convert the yaml to json
+yq r -j ${SOURCE_PIPELINE_ID}.yaml | tee ${SOURCE_PIPELINE_ID}.json
+
 # Remove the hooks
-yq 'del(. | .hooks)' $SOURCE_PIPELINE_ID.yaml > "${TARGET_PIPELINE_ID}.yaml"
+jq 'del(. | .hooks)' $SOURCE_PIPELINE_ID.json > "${TARGET_PIPELINE_ID}.json"
 
 # Add the token url
-yq -r '.stages[] | select( .inputs[0].type=="git") | .inputs[0].url' $SOURCE_PIPELINE_ID.yaml |\
+jq -r '.stages[] | select( .inputs[0].type=="git") | .inputs[0].url' $SOURCE_PIPELINE_ID.json |\
 while IFS=$'\n\r' read -r input_gitrepo 
 do
   token_url=$(cat ${SOURCE_PIPELINE_ID}_inputsources.json | jq -r --arg git_repo "$input_gitrepo" '.[] | select( .repo_url==$git_repo ) | .token_url')
   echo "$input_gitrepo => $token_url"
 
   # Add a token field/line for input of type git and url being $git_repo
-  cp -f $TARGET_PIPELINE_ID.yaml tmp-$TARGET_PIPELINE_ID.yaml
+  cp -f $TARGET_PIPELINE_ID.json tmp-$TARGET_PIPELINE_ID.json
 
-  yq -r --yaml-output --arg input_gitrepo "$input_gitrepo" --arg token_url "$token_url" '.stages[] | if ( .inputs[0].type=="git" and .inputs[0].url==$input_gitrepo) then  .inputs[0]=(.inputs[0] + { "token": $token_url}) else . end' tmp-$TARGET_PIPELINE_ID.yaml | yq -s --yaml-output  '{"stages": .}' > $TARGET_PIPELINE_ID.yaml
+  jq -r --arg input_gitrepo "$input_gitrepo" --arg token_url "$token_url" '.stages[] | if ( .inputs[0].type=="git" and .inputs[0].url==$input_gitrepo) then  .inputs[0]=(.inputs[0] + { "token": $token_url}) else . end' tmp-$TARGET_PIPELINE_ID.json | jq -s '{"stages": .}' > ${TARGET_PIPELINE_ID}.json
   
 done
 
-# Add the pipeline properties in the target YAML
-yq --yaml-output '.properties | {"properties": .}' $SOURCE_PIPELINE_ID.yaml >> $TARGET_PIPELINE_ID.yaml
+# Add the pipeline properties in the target
+cp -f $TARGET_PIPELINE_ID.json tmp-$TARGET_PIPELINE_ID.json
+jq --slurpfile sourcecontent ./${SOURCE_PIPELINE_ID}.json '.stages | {"stages": ., "properties": $sourcecontent[0].properties }' ./tmp-${TARGET_PIPELINE_ID}.json > ${TARGET_PIPELINE_ID}.json
 
-cat $TARGET_PIPELINE_ID.yaml
+yq r $TARGET_PIPELINE_ID.json | tee $TARGET_PIPELINE_ID.yaml
 
 # Include the yaml as rawcontent (ie needs to replace cr by \n and " by \" )
 echo '{}' | jq --rawfile yaml $TARGET_PIPELINE_ID.yaml '{"config": {"format": "yaml","content": $yaml}}' > ${TARGET_PIPELINE_ID}_configuration.json
@@ -63,7 +67,7 @@ curl -H "Authorization: $BEARER_TOKEN" -H "Accept: application/json" $PIPELINE_A
 
 # echoing the secured properties (pipeline and stage) that can not be valued there
 echo "The following pipeline secure properties needs to be updated with appropriate values:"
-yq -r '.properties[] | select(.type=="secure") | .name' ${TARGET_PIPELINE_ID}.yaml
+jq -r '.properties[] | select(.type=="secure") | .name' ${TARGET_PIPELINE_ID}.json
 
 echo "The following stage secure properties needs to be updated with appropriate values:"
-yq -r '.stages[] | . as $stage | .properties // [] | .[] | select(.type=="secure") | [$stage.name] + [.name] | join(" - ")' ${TARGET_PIPELINE_ID}.yaml
+jq -r '.stages[] | . as $stage | .properties // [] | .[] | select(.type=="secure") | [$stage.name] + [.name] | join(" - ")' ${TARGET_PIPELINE_ID}.json
