a4e0dd291d71ef73e4c8cf7cb08302c89879d104
Benoit Jaunin <jaunin.b@fr.ibm.com>
2020-09-22 09:15:03 +0200

    Restore BUILD_ARGS

--- ic_cr_build_vs_buildkit_buildctl/build_image_using_buildkit.sh
+++ ic_cr_build_vs_buildkit_buildctl/build_image_using_buildkit.sh
@@ -91,7 +91,7 @@ set -x
 #ibmcloud cr build -f ${DOCKER_ROOT}/${DOCKER_FILE} -t ${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG} ${EXTRA_BUILD_ARGS} ${DOCKER_ROOT}
 buildctl build \
     --frontend dockerfile.v0 --opt filename=${DOCKER_FILE} --local dockerfile=${DOCKER_ROOT} \
-    --local context=${DOCKER_ROOT} \
+    ${BUILD_ARGS} --local context=${DOCKER_ROOT} \
     --import-cache type=registry,ref=${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME} \
     --output type=image,name="${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}",push=true
 set +x
